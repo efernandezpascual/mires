f7fdcea0ae1d8b5e622a465b3eb3d2774599a9fb
efernandezpascual <61799380+efernandezpascual@users.noreply.github.com>
2020-03-31 23:02:47 +0200

    figure and table structure decided

---
+++ src/final analysis.R
@@ -0,0 +1,152 @@
+library(tidyverse); library(lubridate)
+load("results/clean_data/temperatures.RData")
+
+# Calculations
+
+## Bioclimatic variables US Geological Survey, WorlClim
+
+### Bio 2 Annual Mean Diurnal Range
+
+temperatures %>%
+  group_by(Logger, Day = floor_date(Time, "day")) %>%
+  summarise(Tmax = max(Temperature), 
+            Tmin = min(Temperature),
+            Tdif = Tmax - Tmin) %>%
+  group_by(Logger) %>%
+  summarise(Mean.Diurnal.Range = mean(Tdif)) -> bio2
+
+### Bio 5 Max Temperature of Warmest Month
+
+temperatures %>%
+  group_by(Logger, Day = floor_date(Time, "day")) %>%
+  summarise(Tmax = max(Temperature)) %>%
+  mutate(Month = month(Day)) %>%
+  group_by(Logger, Month) %>%
+  summarise(Max.Temperature.of.Warmest.Month = mean(Tmax)) %>%
+  group_by(Logger) %>%
+  filter(Max.Temperature.of.Warmest.Month == max(Max.Temperature.of.Warmest.Month)) %>%
+  select(-Month) -> bio5
+
+### Bio 6 Min Temperature of the Coldest Month
+
+temperatures %>%
+  group_by(Logger, Day = floor_date(Time, "day")) %>%
+  summarise(Tmin = min(Temperature)) %>%
+  mutate(Month = month(Day)) %>%
+  group_by(Logger, Month) %>%
+  summarise(Min.Temperature.of.Coldest.Month = mean(Tmin)) %>%
+  group_by(Logger) %>%
+  filter(Min.Temperature.of.Coldest.Month == min(Min.Temperature.of.Coldest.Month)) %>%
+  select(-Month) -> bio6
+
+### Merge and calculate Bio 7 Annual Temperature Range
+
+bio2 %>%
+  merge(bio5) %>%
+  merge(bio6) %>%
+  mutate(Temperature.Annual.Range = 
+           Max.Temperature.of.Warmest.Month -
+           Min.Temperature.of.Coldest.Month) -> bioclimatics
+
+## Paired T-tests
+
+t.testEFP <- function(df) 
+{
+  t <- t.test(df$Waterlogged, df$Dry, alternative = "less", paired = TRUE)
+  data.frame(t$statistic,
+             t$p.value,
+             t$estimate)
+}
+
+t.testEFPgreater <- function(df) 
+{
+  t <- t.test(df$Waterlogged, df$Dry, alternative = "greater", paired = TRUE)
+  data.frame(t$statistic,
+             t$p.value,
+             t$estimate) # In the case of the minimum temperature is the opposite hypothesis
+}
+
+rbind(
+  merge(header, bioclimatics, by = "Logger") %>% 
+    gather(Trait, Value, Mean.Diurnal.Range:Temperature.Annual.Range) %>%
+    select(Site, Groundwater, Value, Trait, Value) %>%
+    spread(Groundwater, Value) %>%
+    filter(! Trait %in% "Min.Temperature.of.Coldest.Month") %>%
+    group_by(Trait) %>%
+    do(t.testEFP(.)),
+  merge(header, bioclimatics, by = "Logger") %>% 
+    gather(Trait, Value, Mean.Diurnal.Range:Temperature.Annual.Range) %>%
+    select(Site, Groundwater, Value, Trait, Value) %>%
+    spread(Groundwater, Value) %>%
+    filter(Trait %in% "Min.Temperature.of.Coldest.Month") %>%
+    group_by(Trait) %>%
+    do(t.testEFPgreater(.))) -> ttests
+
+#----------------------------------------------------------------------------
+
+# Tables
+
+## Table 1 - Study sites
+
+temperatures %>%
+  group_by(Logger) %>%
+  summarise(Start = min(Time),
+            End = max(Time),
+            Length = End - Start,
+            Length = round(as.numeric(Length), 0)) -> 
+  recording.period
+
+merge(header, recording.period) %>%
+  group_by(Site, Mire, Elevation, Length) %>%
+  summarise(Latitude = mean(Latitude), 
+            Longitude = mean (Longitude)) %>%
+  select(Site, Mire, Elevation, Latitude, Longitude, Length) %>%
+  rename(Habitat = Mire,
+         `Elevation (m)` = Elevation,
+         `Latitude (ºC)` = Latitude,
+         `Longitude (ºc)` = Longitude,
+         `Records (days)` = Length) -> table1
+
+## Table 2 - Bioclimatic variables
+
+merge(header, bioclimatics) %>%
+  select(Site, Groundwater, Mean.Diurnal.Range:Temperature.Annual.Range) %>%
+  gather(Trait, Value, Mean.Diurnal.Range:Temperature.Annual.Range) %>%
+  mutate(Trait = gsub("\\.", " ", Trait), 
+         Value = round(Value, 2)) %>%
+  mutate(Column = paste(Trait, Groundwater, sep = "\n")) %>%
+  select(-c(Trait, Groundwater)) %>%
+  spread(Column, Value) -> table2 # add header with https://rdrr.io/cran/kableExtra/man/add_header_above.html
+
+# Figures
+
+## Figure 1 - Datalogger records
+
+merge(temperatures, header) %>%
+  arrange(Elevation, Site) %>%
+  mutate(Facet = paste(Elevation, Site, sep = " m a.s.l. - ")) -> recordsdf
+
+recordsdf %>%
+  select(Elevation, Facet) %>%
+  unique %>%
+  arrange(-Elevation) %>%
+  pull(Facet) -> Order
+
+recordsdf$Facet <- factor(recordsdf$Facet, levels = Order)
+
+recordsdf %>%
+  ggplot(aes(x = Time, y = Temperature, color = Groundwater)) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  geom_line(alpha = 0.85) +
+  facet_wrap(~ Facet, ncol = 1) +
+  xlab("Time (hourly records)") + ylab("Temperature (ºC)") +
+  scale_color_manual(values = c("darkorchid", "gold")) +
+  theme_tufte() +
+  theme(text = element_text(size = 10),
+        legend.position = "top", 
+        legend.justification = "center",
+        legend.title = element_blank()) +
+  guides(colour = guide_legend(override.aes = list(alpha=1))) -> plot1
+
+ggsave(filename = "results/figures/Fig1.png", plot = plot1,
+       path = NULL, scale = 1, width = 80, height = 250, units = "mm", dpi = 600)
---
+++ src/read loggers.R
@@ -0,0 +1,150 @@
+library(tidyverse); library(sp); library(lubridate)
+
+# Function reads and formats GP5WSHELL csv export files
+
+read_gp5 <- function(x)
+{
+  require(tidyverse)
+  read.csv(x, skip = 2, header = TRUE) %>% # Rows to skip depend on GP5WShell version!!
+    dplyr::rename(Temperature = X.1.oC) %>%
+    mutate(Time = as.POSIXct(Time, 
+                             format =" %d.%m.%Y %H:%M:%S", 
+                             tz = "UTC"),
+           Logger = gsub("\\_.*", "" , x)) %>%
+    dplyr::select(Logger, Time, Temperature)
+}
+
+# Functions convert coordinates to decimal degrees
+
+decdeg30T <- function(x) { x -> o1 
+  o1 %>% dplyr::select(ED50X, ED50Y) -> o2
+  SpatialPoints(o2, proj4string = CRS("+proj=utm +zone=30T +datum=WGS84")) -> o3
+  spTransform(o3, CRS("+proj=longlat +datum=WGS84")) %>% data.frame -> o4
+  colnames(o4) <- c("Longitude", "Latitude")
+  o1 %>% dplyr::select(-c(ED50X, ED50Y)) -> o5
+  cbind(o5, o4)
+}
+
+decdeg29T <- function(x) { x -> o1 
+  o1 %>% dplyr::select(ED50X, ED50Y) -> o2
+  SpatialPoints(o2, proj4string = CRS("+proj=utm +zone=29T +datum=WGS84")) -> o3
+  spTransform(o3, CRS("+proj=longlat +datum=WGS84")) %>% data.frame -> o4
+  colnames(o4) <- c("Longitude", "Latitude")
+  o1 %>% dplyr::select(-c(ED50X, ED50Y)) -> o5
+  cbind(o5, o4)
+}
+
+# Read logger header data
+
+read.table("data/dataloggers/Loggers.txt", sep = "\t", header = TRUE) %>%
+  mutate(Installed = as.POSIXct(Installed, 
+                                format ="%d/%m/%Y", 
+                                tz = "UTC")) -> df1
+
+df1 %>% 
+  filter(ED50Zone == "29T") %>%
+  decdeg29T -> df29T
+
+df1 %>% 
+  filter(ED50Zone == "30T") %>%
+  decdeg30T -> df30T
+
+rbind(df29T, df30T) %>%
+  select(-c(Name, ED50Zone)) -> header
+
+header %>%
+  group_by(Site) %>%
+  summarise(Elevation = round(mean(Elevation), 0)) -> average.elevation
+
+header %>% select(-Elevation) %>% 
+  merge(average.elevation) -> header # Only one elevation value per site
+
+# Map of the loggers
+
+header %>%
+  ggplot(aes(x = Longitude, y = Latitude)) + 
+  labs(x = "Longitude (º)", y = "Latitude (º)") +
+  geom_polygon(data = map_data("world"), aes(x = long, y = lat, group = group), 
+               color = "black", fill = "grey") + 
+  geom_point(aes(color = "indianred"), alpha = 0.5, size = 3, show.legend = FALSE) +
+  geom_text(aes(x = -3, y = 42, label = "Spain"), size = 4) +
+  geom_text(aes(x = 1, y = 44.8, label = "France"), size = 4) +
+  geom_text(aes(x = -7.7, y = 40.8, label = "Portugal"), size = 4) +
+  geom_text(aes(x = -5.5, y = 45.5, label = "Atlantic"), size = 3) +
+  coord_cartesian(xlim = c(-10, 2), ylim = c(40, 46)) +
+  theme(legend.position = "top", legend.justification = "center",
+        panel.background = element_rect(fill = "darkgrey", colour = "darkgrey"),
+        legend.text = element_text(size = 8), legend.title = element_blank())
+
+
+# Read and bind all loggers, merge with logger header data
+# Could not use filepath, 
+# " Error in file(file, "rt") : cannot open the connection"
+
+wd <- getwd()
+setwd(paste(wd, "/data/dataloggers", sep = ""))
+do.call(rbind, lapply(list.files(pattern = "*.csv"), read_gp5)) %>% 
+  merge(header, by = "Logger") %>%
+  na.omit -> logs
+setwd(wd)
+
+# Keep only records with two working loggers per site
+
+logs %>% 
+  group_by(Site, Groundwater) %>%
+  summarise(Last.record = max(Time)) %>%
+  group_by(Site) %>%
+  summarise(End = min(Last.record)) -> ends
+
+logs %>% 
+  group_by(Site, Groundwater) %>%
+  summarise(First.record = min(Time)) %>%
+  group_by(Site) %>%
+  summarise(Start = max(First.record)) -> starts
+
+merge(starts, ends) -> period
+
+merge(logs, period, by = "Site") %>%
+  group_by(Logger) %>%
+  filter(Time > (Installed + 60*60*24*7)) %>% # Remove records up to one week after installation
+  filter(Time >= Start & Time <= End) -> # Remove logs with only one working logger per site
+  logs.clean
+
+# Visual check of the temperatures
+
+logs.clean %>%
+  ggplot(aes(x = Time, y = Temperature, color = Groundwater)) +
+  geom_line() +
+  facet_wrap(~ Site)
+
+# Waterlogged logger at Los Cándanos looks like it sunk into the bog
+
+logs.clean %>%
+  filter(Time < "2014-10-25") %>%
+  filter(Site == "Los Cándanos" & Groundwater == "Waterlogged") %>%
+  group_by(day = floor_date(Time, "day")) %>%
+  summarise(dif = max(Temperature) - min(Temperature)) %>%
+  ggplot(aes(x = day, y = dif)) + 
+  geom_line() # Problem seems to be around 2014-10-20
+
+# Bacause of the few records I'll remove this site
+
+logs.clean %>% 
+  filter(! Site == "Los Cándanos") -> logs.clean2
+
+# Visual check again
+
+logs.clean2 %>%
+  ggplot(aes(x = Time, y = Temperature, color = Groundwater)) +
+  geom_line() +
+  facet_wrap(~ Site)
+
+# Final clean file
+
+logs.clean2 %>%
+  dplyr::select(Logger, Time, Temperature) ->
+  temperatures
+
+write.csv(temperatures, "results/clean_data/temperatures.csv", row.names = FALSE)
+write.csv(header, "results/clean_data/header.csv", row.names = FALSE)
+save(temperatures, header, file = "results/clean_data/temperatures.RData")
