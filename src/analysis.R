5ea97e167efffefd974ef25af30df59a3426076c
efernandezpascual <61799380+efernandezpascual@users.noreply.github.com>
2020-08-29 13:05:34 +0200

    Revert "map of the sites"
    This reverts commit 5e6ba1be55006768cd3e0dd015b7150cf954eb21.

--- src/analysis.R
+++ src/analysis.R
@@ -157,89 +157,9 @@ bio %>%
          p = round(p, 3),
          R2 = round(R2, 2)) -> table3
 
-#----------------------------------------------------------------------------
-
 # Figures
 
-## Figure 1 - Map
-
-library(raster)
-
-### Get map of Spain
-
-spaingeo <- getData("GADM", country = "spain", level = 1)
-spaindf <- fortify(spaingeo)
-
-spainalt <- getData("alt", country = "spain", level = 1)
-altdf  <- data.frame(rasterToPoints(spainalt))
-colnames(altdf) = c("lon", "lat", "alt")
-altdf %>% filter(alt < 2650) -> altdf
-
-### Draw map of Spain
-
-header %>%
-  group_by(Site, Mire) %>%
-  summarise(long = mean(Longitude), lat = mean(Latitude)) %>%
-  ggplot(aes(long, lat)) +
-  geom_tile(data = altdf, aes(x = lon, y = lat, fill = alt)) +
-  scale_fill_gradient(name = "Elevation (m asl)", low = "grey96", high = "black",
-                      breaks = c(0, 2600)) +
-  geom_blank(data = spaindf, aes(x = long, y = lat)) +
-  geom_map(data = spaindf, map = spaindf,
-           aes(group = group, map_id = id),
-           fill = NA, color = "black", size = .3) +
-  geom_point(size = 2) +
-  ggrepel::geom_label_repel(aes(label = Site), box.padding = .6, size = 2.5) +
-  labs(x = "Longitude (º)", y = "Latitude (º)") +
-  geom_text(aes(x = -6.1, y = 43.75, label = "Cantabrian Sea"), size = 3.5) +
-  geom_text(aes(x = -6.1, y = 43.4, label = "Asturies"), size = 5) +
-  geom_text(aes(x = -5.1, y = 42.9, label = "León"), size = 5) +
-  scale_x_continuous(limits = c(-6.5, -4.7), expand = c(0, 0)) +
-  scale_y_continuous(limits = c(42.8, 43.9), expand = c(0, 0)) +
-  ggthemes::theme_map() +
-  theme(panel.background = element_rect(color = "black", fill = "grey96"),
-        legend.position = "bottom",
-        legend.direction = "horizontal",
-        legend.background = element_blank(),
-        legend.box.background = element_rect(colour = NA),
-        legend.title = element_text(size = 10),
-        legend.text = element_text(size = 8),
-        legend.justification = "left",
-        legend.margin = margin(0, 0, 0, 0),
-        legend.box.margin = margin(-10, -10, 0, 0),
-        plot.margin = unit(c(0, 0.1, 0, 0), "cm"),
-        axis.title = element_blank()) -> g1
-
-### Draw map of Europe
-
-ggplotGrob(
-  ggplot() +
-    geom_polygon(data = map_data("world"), aes(x = long, y = lat, group = group), 
-                 color = "black", fill = "grey", size = 0.25) +
-    geom_path(data = data.frame(long = c(-6.5, -6.5, -4.7, -4.7, -6.5),
-                                lat = c(42.8, 43.9, 43.9, 42.8, 42.8)), 
-              aes(x = long, y = lat), size = 0.25) +
-    labs(x = "Longitude (º)", y = "Latitude (º)") +
-    coord_cartesian(xlim = c(-8.8, 25), ylim = c(36.5, 58.5)) +
-    ggthemes::theme_map() +
-    theme(panel.background = element_rect(color = "grey96", fill = "grey96"),
-          legend.position = "top", legend.title = element_blank(),
-          panel.border = element_rect(colour = "black", fill = NA, size = 1))) -> g2
- 
-### Put together
-
-g1 +
-  annotation_custom(grob = g2, xmin = -5.54, xmax = -4.7,
-                    ymin = 43.445, ymax = 43.9) -> plot1a
-
-### Add picture
-
-img1 <- png::readPNG("data/riotuertu.png")
-plot1b <- grid::rasterGrob(img1, width = unit(85, "mm"), height = unit(85, "mm"), just = "centre") 
-
-cowplot::plot_grid(plot1a, plot1b, ncol = 2, labels = c("a", "b")) -> plot1
-
-## Figure 2 - Datalogger records
+## Figure 1 - Datalogger records
 
 merge(temperatures, header) %>%
   arrange(Elevation, Site) %>%
@@ -267,9 +187,9 @@ recordsdf %>%
         legend.title = element_blank(),
         axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
   guides(colour = guide_legend(override.aes = list(alpha = 1))) +
-  scale_color_manual(values = c("red3", "turquoise3")) -> plot2
+  scale_color_manual(values = c("red3", "turquoise3")) -> plot1
 
-## Fig 3 - Effect sizes
+## Fig 2 - Effect sizes
 
 merge(header[, 1:8], bioclimatics) %>%
   select(Site, Groundwater, `Diurnal range`:`Annual range`) %>%
@@ -292,9 +212,9 @@ merge(header[, 1:8], bioclimatics) %>%
         legend.title = element_blank(),
         strip.placement = "outside",
         axis.title = element_blank()) +
-  scale_fill_manual(values = c("red3", "turquoise3")) -> plot3
+  scale_fill_manual(values = c("red3", "turquoise3")) -> plot2
 
-## Fig 4 Chelsa
+## Fig 3 Chelsa
 
 bio %>%
   ggplot(aes(CHELSA, Loggers, color = Groundwater)) + 
@@ -309,20 +229,17 @@ bio %>%
         legend.justification = "center",
         legend.title = element_blank(),
         strip.placement = "outside") +
-  scale_color_manual(values = c("red3", "turquoise3")) -> plot4
+  scale_color_manual(values = c("red3", "turquoise3")) -> plot3
 
 # Save
 
-save(ttests, table1, table2, table3, plot1, plot2, plot3, plot4, file = here::here("results", "MSoutput.RData"))
+save(ttests, table1, table2, table3, plot1, plot2, plot3, file = here::here("results", "MSoutput.RData"))
 
 ggsave(plot1, file = here::here("doc", "manuscript", "Fig1.png"), 
-       path = NULL, scale = 1, width = 170, height = 85, units = "mm", dpi = 600)
-
-ggsave(plot2, file = here::here("doc", "manuscript", "Fig2.png"), 
        path = NULL, scale = 1, width = 170, height = 120, units = "mm", dpi = 600)
 
-ggsave(plot3, file = here::here("doc", "manuscript", "Fig3.png"), 
+ggsave(plot2, file = here::here("doc", "manuscript", "Fig2.png"), 
        path = NULL, scale = 1, width = 170, height = 70, units = "mm", dpi = 600)
 
-ggsave(plot4, file = here::here("doc", "manuscript", "Fig4.png"), 
+ggsave(plot3, file = here::here("doc", "manuscript", "Fig3.png"), 
        path = NULL, scale = 1, width = 170, height = 150, units = "mm", dpi = 600)
